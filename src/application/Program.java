6fa5d074518bffcbb787686f393313a04aed0a2e
Duduxs <duduxss3@gmail.com>
2020-03-11 21:14:24 -0300

    Programn and Department class implemented/tested

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department d = new Department(5,"Eduardo");
+		
+		System.out.println(d);
+	}
+}
