a9100c166ddc7441b1e8461cb769d62dd4b7277f
Duduxs <duduxss3@gmail.com>
2020-03-11 21:38:07 -0300

    Implemented Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department d = new Department(5,"Eduardo");
+
 		
-		System.out.println(d);
+		Department d = new Department(1,"Books");
+		Seller s = new Seller(1,"Eduardo","duduxss3@gmail.com",new Date(),3700.00,d);
+		
+		System.out.println(s);
+		
 	}
 }
